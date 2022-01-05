55e15e96561393be6a1ec91f7388cf0e8d34b762
HeoJiye <95454039+Heoze@users.noreply.github.com>
2022-01-05 11:22:47 +0900

    코드 정리함

--- view1.lua
+++ view1.lua
@@ -10,238 +10,265 @@ local scene = composer.newScene()
 function scene:create( event )
 	local sceneGroup = self.view
 
-	local BGUI = display.newGroup();
-	local background = display.newRect(BGUI, display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
-	local w,h = display.contentWidth, display.contentHeight/2
+-- 변수 선언부 --
 
-	physics.start()
-    physics.setGravity( 0, 0 )
+	-- BG
+		local BGUI = display.newGroup();
+		local background = display.newRect(BGUI, display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
+		local w,h = display.contentWidth, display.contentHeight/2
 
-	local sky = display.newImageRect(BGUI, "Content/sky.png", display.contentWidth, display.contentHeight)
-	sky.x, sky.y = display.contentWidth/2, display.contentHeight/2
+		local sky = display.newImageRect(BGUI, "Content/sky.png", display.contentWidth, display.contentHeight)
+		sky.x, sky.y = display.contentWidth/2, display.contentHeight/2
 
-	local ground = display.newImageRect(BGUI, "Content/ground.png", display.contentWidth, 300)
-	ground.x, ground.y = display.contentWidth/2, display.contentHeight-150
+		local ground = display.newImageRect(BGUI, "Content/ground.png", display.contentWidth, 300)
+		ground.x, ground.y = display.contentWidth/2, display.contentHeight-150
 
-    local score = 0
-    local showScore = display.newText(score, display.contentWidth*0.25, display.contentHeight*0.15, 500, 100)
-    showScore.align = "right"
-    showScore:setFillColor(0)
-    showScore.size = 80
+    -- UI
 
-    local function scoreUp ( event )
-    	score = score + 1
-    	showScore.text = score
-    end
+    	-- BGM
+		local music = audio.loadStream( "Content/music1.ogg" )
+		local bgMusic
+		local soundOn = 0
+		local bgmUI = {}
+		bgmUI[0] = display.newImageRect("Content/on.png", 55, 55)
+		bgmUI[0].x, bgmUI[0].y = 1240, 40
+		bgmUI[0].alpha = 0
+		bgmUI[1] = display.newImageRect("Content/off.png", 55, 55)
+		bgmUI[1].x, bgmUI[1].y = 1240, 40
 
-	local music = audio.loadStream( "Content/music1.ogg" )
-	local bgMusic
-	local soundOn = 0
-	local bgmUI = {}
-	bgmUI[0] = display.newImageRect("Content/on.png", 55, 55)
-	bgmUI[0].x, bgmUI[0].y = 1240, 40
-	bgmUI[0].alpha = 0
-	bgmUI[1] = display.newImageRect("Content/off.png", 55, 55)
-	bgmUI[1].x, bgmUI[1].y = 1240, 40
-
-	local playUI = {}
-	playUI[0] = display.newImageRect("Content/play.png", 55, 55)
-	playUI[0].x, playUI[0].y = 1180, 40
-	playUI[0].alpha = 0
-	playUI[1] = display.newImageRect("Content/stop.png", 55, 55)
-	playUI[1].x, playUI[1].y = 1180, 40
-
-	local UI = {}
-	UI[0] = display.newRect(640, 360, 600, 300)
-	UI[0]:setFillColor(0.5)
-	UI[1] = display.newImage("Content/start.png") 
-	UI[1].x, UI[1].y = 640, 400
-	UI[2] = display.newImageRect("Content/x.png", 30, 30)
-	UI[2].x, UI[2].y = 920, 230
-
-	for i = 0, #UI do
-		UI[i].alpha = 0
-	end
-
-	local dino_sheet = graphics.newImageSheet( "Content/player.png", { width = 240, height = 240, numFrames = 6 })
-	local sepuencesData =
-	{
-		{ name = "stand",start = 1, count = 1},
-		{ name = "run",  start = 1, count = 3, time = 400 },
-		{ name = "jump", start = 4, count = 1, loopCount = 1, time = 1000},
-		{ name = "slide", start = 5, count = 1, time = 400},
-		{ name = "hurt", start = 6, count = 1}
-	}
-
-	local dino = display.newSprite( dino_sheet, sepuencesData )
-	dino.x, dino.y = display.contentWidth*0.2, display.contentHeight*0.5
-
-    local obstacle = {}
-	obstacle[1] = display.newImageRect("Content/bone1.png", 69, 95)
-	obstacle[2] = display.newImageRect("Content/bone2.png", 131, 115)
-	obstacle[3] = display.newImageRect("Content/bone3.png", 117, 84)
-	obstacle[4] = display.newImageRect("Content/Ptero.png", 276, 119)
-
-	for i = 1, 3, 1 do 
-		obstacle[i].x, obstacle[i].y = display.contentWidth+200, display.contentHeight-280
-	end
-	obstacle[4].x, obstacle[4].y = display.contentWidth+200, 250
-
-	-- 타이머들
-	local spawnTimer
-	local resetTimer
-	local cooltime
-	local obs_idx
-
-    local function dino_spriteListenr( event )
-    	if event.phase == "began" then
-    		--if dino.sequence == "jump" then
-
-	    	--else
-	    	if dino.sequence == "hurt" then
-	    		dino:setFillColor(0.75, 0.5, 0.5)
-	    		timer.cancel( scoreEvent )
-	    		--display.remove( jumpButton )
-	    	end
-    	elseif event.phase == "ended" then
-	    	dino:setSequence( "run" )
-	    	dino:play()
-    	end
-    end
-    dino:addEventListener( "sprite", dino_spriteListenr )
-
-	local function soundONOFF( ... )
-		if soundOn == 1 then
-			soundOn = 0
-			bgmUI[0].alpha = 0
-			bgmUI[1].alpha = 1
-			audio.stop(bgMusic)
-		else
-			soundOn = 1
-			bgmUI[0].alpha = 1
-			bgmUI[1].alpha = 0
-			bgMusic = audio.play(music, { channel=1, loops=-1, fadein=5000 })
-		end
-	end
-	bgmUI[0]:addEventListener("tap", soundONOFF)
-	bgmUI[1]:addEventListener("tap", soundONOFF)
-
-	local function tapPlay( ... )
+		-- PLAY
+		local playUI = {}
+		playUI[0] = display.newImageRect("Content/play.png", 55, 55)
+		playUI[0].x, playUI[0].y = 1180, 40
 		playUI[0].alpha = 0
-		playUI[1].alpha = 1
+		playUI[1] = display.newImageRect("Content/stop.png", 55, 55)
+		playUI[1].x, playUI[1].y = 1180, 40
+
+		local UI = {}
+		UI[0] = display.newRect(640, 360, 600, 300)
+		UI[0]:setFillColor(0.5)
+		UI[1] = display.newImage("Content/start.png") 
+		UI[1].x, UI[1].y = 640, 400
+		UI[2] = display.newImageRect("Content/x.png", 30, 30)
+		UI[2].x, UI[2].y = 920, 230
 
 		for i = 0, #UI do
 			UI[i].alpha = 0
 		end
 
-		scoreEvent = timer.performWithDelay( 250, scoreUp, 0 )
-		dino:setSequence( "run" )
-	    dino:play()
+    -- SCORE
+	    local score = 0
+	    local showScore = display.newText(score, display.contentWidth*0.25, display.contentHeight*0.15, 500, 100)
+	    showScore.align = "right"
+	    showScore:setFillColor(0)
+	    showScore.size = 80
 
-	    physics.start()
-	    timer.resume(spawnTimer)
-	    timer.resume(resetTimer)
-	    timer.resume(scoreEvent)
-	end
+	    local scoreEvent
 
-	local function tapStop( ... )
-		playUI[0].alpha = 1
-		playUI[1].alpha = 0
+    -- DINO
+		local dino_sheet = graphics.newImageSheet( "Content/player.png", { width = 240, height = 240, numFrames = 6 })
+		local sepuencesData =
+		{
+			{ name = "stand",start = 1, count = 1},
+			{ name = "run",  start = 1, count = 3, time = 400 },
+			{ name = "jump", start = 4, count = 1, loopCount = 1, time = 1000},
+			{ name = "slide", start = 5, count = 1, time = 400},
+			{ name = "hurt", start = 6, count = 1}
+		}
 
-		for i = 0, #UI do
-			UI[i].alpha = 1
+		local dino = display.newSprite( dino_sheet, sepuencesData )
+		dino.x, dino.y = display.contentWidth*0.2, display.contentHeight*0.5
+
+	-- OBSTACLE
+		local obstacleGroup = display.newGroup();
+	    local obstacle = {}
+		obstacle[1] = display.newImageRect(obstacleGroup, "Content/bone1.png", 69, 95)
+		obstacle[2] = display.newImageRect(obstacleGroup, "Content/bone2.png", 131, 115)
+		obstacle[3] = display.newImageRect(obstacleGroup, "Content/bone3.png", 117, 84)
+		obstacle[4] = display.newImageRect(obstacleGroup, "Content/Ptero.png", 276, 119)
+
+		for i = 1, 3, 1 do 
+			obstacle[i].x, obstacle[i].y = display.contentWidth+200, display.contentHeight-280
+		end
+		obstacle[4].x, obstacle[4].y = display.contentWidth+200, 250
+
+		-- Timer
+		local spawnTimer
+		local resetTimer
+
+		local cooltime
+		local obs_idx
+
+
+-- 함수 선언부 --
+
+    -- UI
+
+    	-- BGM
+		local function soundONOFF( ... )
+			if soundOn == 1 then
+				soundOn = 0
+				bgmUI[0].alpha = 0
+				bgmUI[1].alpha = 1
+				audio.stop(bgMusic)
+			else
+				soundOn = 1
+				bgmUI[0].alpha = 1
+				bgmUI[1].alpha = 0
+				bgMusic = audio.play(music, { channel=1, loops=-1, fadein=5000 })
+			end
+		end
+		bgmUI[0]:addEventListener("tap", soundONOFF)
+		bgmUI[1]:addEventListener("tap", soundONOFF)
+
+		-- PLAY
+		local function tapPlay( ... )
+			playUI[0].alpha = 0
+			playUI[1].alpha = 1
+
+			for i = 0, #UI do
+				UI[i].alpha = 0
+			end
+
+			dino:setSequence( "run" )
+		    dino:play()
+
+		    physics.start()
+		    timer.resume(spawnTimer)
+		    timer.resume(resetTimer)
+		    timer.resume(scoreEvent)
 		end
 
-		dino:setSequence( "stand" )
-	    dino:play()
+		local function tapStop( ... )
+			playUI[0].alpha = 1
+			playUI[1].alpha = 0
 
-	    physics.pause()
-	    timer.pause(spawnTimer)
-	    timer.pause(resetTimer)
-	    timer.pause(scoreEvent)
-	end
+			for i = 0, #UI do
+				UI[i].alpha = 1
+			end
 
-	local function tapX( ... )
-		for i = 0, #UI do
-			UI[i].alpha = 0
+			dino:setSequence( "stand" )
+		    dino:play()
+
+		    physics.pause()
+		    timer.pause(spawnTimer)
+		    timer.pause(resetTimer)
+		    timer.pause(scoreEvent)
 		end
-	end
 
-	playUI[0]:addEventListener("tap", tapPlay)
-	playUI[1]:addEventListener("tap", tapStop)
-	UI[1]:addEventListener("tap", tapPlay)
-	UI[2]:addEventListener("tap", tapX)
+		local function tapX( ... )
+			for i = 0, #UI do
+				UI[i].alpha = 0
+			end
+		end
 
-	-- 장애물과 충돌했을 때
-	-- dino:setSequence( "hurt" )
-	-- dino:play()
-	-- composer.setVariable("score", score)
-	-- composer.gotoScene("end")
+		playUI[0]:addEventListener("tap", tapPlay)
+		playUI[1]:addEventListener("tap", tapStop)
+		UI[1]:addEventListener("tap", tapPlay)
+		UI[2]:addEventListener("tap", tapX)
 
-    local function playerDown( event )
-    	transition.to( dino, { time=500,  y=(dino.y+150) } )
-    end
-
-	local function onKeyJumpEvent( event )
-	 	if ( event.keyName == "space" ) and ( event.phase == "down" ) and (dino.y == h) then
-	 		transition.to( dino, { time=500,  y=(dino.y-150), onComplete = playerDown } )
-	 		dino:setSequence( "jump" )
-	 		print("jump")
+    -- SCORE
+	    local function scoreUp ( event )
+	    	score = score + 1
+	    	showScore.text = score
 	    end
-	end
 
-	local function onKeySlideEvent( event )
-	 	if ( event.keyName == "down" ) and ( event.phase == "down" ) then
-	 		transition.pause( dino )
-    		dino.y = display.contentHeight*0.5
-	 		dino:setSequence( "slide" )
-	 		print("slide")
+    -- DINO
+	    local function dino_spriteListenr( event )
+	    	if event.phase == "began" then
+		    	if dino.sequence == "hurt" then
+		    		dino:setFillColor(0.75, 0.5, 0.5)
+		    	end
+	    	elseif event.phase == "ended" then
+		    	dino:setSequence( "run" )
+		    	dino:play()
+	    	end
 	    end
-	    if ( event.keyName == "down" ) and ( event.phase == "up" ) then
- 			dino:setSequence( "run" )
- 		end
- 		dino:play()
-	end
+	    dino:addEventListener( "sprite", dino_spriteListenr )
 
-	Runtime:addEventListener( "key", onKeyJumpEvent )
-	Runtime:addEventListener( "key", onKeySlideEvent )
+	    -- 점프/슬라이드
+	    local function playerDown( event )
+	    	transition.to( dino, { time=500,  y=(dino.y+150) } )
+	    end
 
-	function start()
-		cooltime = math.random(1, 4)--0.5~2초 사이의 간격으로 스폰
-		obs_idx = math.random(1, 4)--1~5번 장애물 중 랜덤선택
-		print("spawn time = "..cooltime)
-		print("obstacle idx is "..obs_idx)
+		local function onKeyJumpEvent( event )
+		 	if ( event.keyName == "space" ) and ( event.phase == "down" ) and (dino.y == h) then
+		 		transition.to( dino, { time=500,  y=(dino.y-150), onComplete = playerDown } )
+		 		dino:setSequence( "jump" )
+		 		print("jump")
+		    end
+		end
 
-		spawnTimer = timer.performWithDelay(cooltime*500, spawn_obstacle)
-	end
+		local function onKeySlideEvent( event )
+		 	if ( event.keyName == "down" ) and ( event.phase == "down" ) then
+		 		transition.pause( dino )
+	    		dino.y = display.contentHeight*0.5
+		 		dino:setSequence( "slide" )
+		 		print("slide")
+		    end
+		    if ( event.keyName == "down" ) and ( event.phase == "up" ) then
+	 			dino:setSequence( "run" )
+	 		end
+	 		dino:play()
+		end
 
-	function obs_reset()--다시 화면 밖으로(초기상태로)
-		print("obstacle.x is out of screen")
+		Runtime:addEventListener( "key", onKeyJumpEvent )
+		Runtime:addEventListener( "key", onKeySlideEvent )
 
-		obstacle[obs_idx]:setLinearVelocity( 0, 0 )
-		physics.removeBody(obstacle[obs_idx])
-		obstacle[obs_idx].x = display.contentWidth+200
 
-		start()
-	end
+		-- 장애물과 충돌했을 때
+		-- dino:setSequence( "hurt" )
+		-- dino:play()
+		-- composer.setVariable("score", score)
+		-- composer.gotoScene("end")
 
-	function spawn_obstacle ()
-		print("spawn obstacle")
+	-- OBSTACLE
+		function obs_start()
+			cooltime = math.random(1, 4)--0.5~2초 사이의 간격으로 스폰
+			obs_idx = math.random(1, 4)--1~5번 장애물 중 랜덤선택
+			print("spawn time = "..cooltime)
+			print("obstacle idx is "..obs_idx)
 
-		physics.addBody( obstacle[obs_idx], "dynamic" )
-		obstacle[obs_idx]:setLinearVelocity( -500, 0 )--장애물 이동
+			spawnTimer = timer.performWithDelay(cooltime*500, spawn_obstacle)
+		end
 
-		resetTimer = timer.performWithDelay(4000, obs_reset)
-	end
-	
-	dino:setSequence( "run" )
-	dino:play()
-	start()
+		function obs_reset()--다시 화면 밖으로(초기상태로)
+			print("obstacle.x is out of screen")
+
+			obstacle[obs_idx]:setLinearVelocity( 0, 0 )
+			physics.removeBody(obstacle[obs_idx])
+			obstacle[obs_idx].x = display.contentWidth+200
+
+			obs_start()
+		end
+
+		function spawn_obstacle ()
+			print("spawn obstacle")
+
+			physics.addBody( obstacle[obs_idx], "dynamic" )
+			obstacle[obs_idx]:setLinearVelocity( -500, 0 )--장애물 이동
+
+			resetTimer = timer.performWithDelay(4000, obs_reset)
+		end
+
+
+-- 함수 호출부 (게임 시작할때 호출하는 함수) --
 	scoreEvent = timer.performWithDelay( 250, scoreUp, 0 )
 
+	dino:setSequence( "run" )
+	dino:play()
+
+	obs_start()
+
+	physics.start()
+	physics.setGravity( 0, 0 )
+
+
+-- 레이어(sceneGroup) 정리 --
 	sceneGroup:insert( BGUI )
     sceneGroup:insert( showScore )
     sceneGroup:insert( dino )
+    sceneGroup:insert( obstacleGroup )
 
    	for i = 0, #bgmUI do sceneGroup:insert( bgmUI[i] ) end
     for i = 0, #UI do sceneGroup:insert( UI[i] ) end
